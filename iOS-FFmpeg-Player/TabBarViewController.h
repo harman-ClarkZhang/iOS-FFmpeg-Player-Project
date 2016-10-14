29500e87b7384667394584de081ea6db75b3ac61
Clark Zhang <clark.zhang@harman.com>
2016-10-14 13:40:36 +0800

    CZ  Create prefixHeader pct file

---
+++ iOS-FFmpeg-Player/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  iOS-FFmpeg-Player
+//
+//  Created by harman on 2016/10/14.
+//  Copyright © 2016年 harman. All rights reserved.
+//
+
+#import <RDVTabBarController/RDVTabBarController.h>
+
+@interface TabBarViewController : RDVTabBarController
+
+@end
--- iOS-FFmpeg-Player/TabBarViewController.m
+++ iOS-FFmpeg-Player/TabBarViewController.m
@@ -1,29 +1,28 @@
 //
-//  ViewController.m
+//  TabBarViewController.m
 //  iOS-FFmpeg-Player
 //
 //  Created by harman on 2016/10/14.
 //  Copyright © 2016年 harman. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "TabBarViewController.h"
 
-@interface ViewController ()
+@interface TabBarViewController ()
 
 @end
 
-@implementation ViewController
+@implementation TabBarViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor yellowColor];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
